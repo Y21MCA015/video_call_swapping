bbc09da9e9e2564edf95cdeed78ca58b2f4c8c43
Annam. Naga Maheswari <nagamaheswariannam@gmail.com>
2026-03-26 22:01:04 +0530

    updated the constants

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -2,11 +2,11 @@
 export const isDevelopment = import.meta.env.MODE === 'development';
 
 export const BASE_API_URL = isDevelopment
-  ? 'http://127.0.0.1:8000/api'
+  ? 'http://192.168.0.133:8000/api'
   : 'https://api.dummy-production.com/api';
 
 export const BASE_WS_URL = isDevelopment
-  ? 'ws://127.0.0.1:8000/ws'
+  ? 'ws://192.168.0.133:8000/ws'
   : 'wss://api.dummy-production.com/ws';
 
 export const ENDPOINTS = {
