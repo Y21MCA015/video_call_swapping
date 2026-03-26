4962717278b512c8d3265f206d12e7fa4ba94db5
Annam. Naga Maheswari <nagamaheswariannam@gmail.com>
2026-03-26 22:37:31 +0530

    removed cahched node modules

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -9,6 +9,8 @@ export const BASE_WS_URL = isDevelopment
   ? 'ws://192.168.0.133:8000/ws'
   : 'wss://video-call-swapping.onrender.com/ws';
 
+
+  
 export const ENDPOINTS = {
   CALL_WS: '/call/',
 };
