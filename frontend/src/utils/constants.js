6335f7bdbd50fe58aec449b69379f8206d756a0d
Annam. Naga Maheswari <nagamaheswariannam@gmail.com>
2026-03-26 22:22:41 +0530

    updated the backend live url in constants

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -3,11 +3,11 @@ export const isDevelopment = import.meta.env.MODE === 'development';
 
 export const BASE_API_URL = isDevelopment
   ? 'http://192.168.0.133:8000/api'
-  : 'https://api.dummy-production.com/api';
+  : 'https://video-call-swapping.onrender.com/api';
 
 export const BASE_WS_URL = isDevelopment
   ? 'ws://192.168.0.133:8000/ws'
-  : 'wss://api.dummy-production.com/ws';
+  : 'wss://video-call-swapping.onrender.com/ws';
 
 export const ENDPOINTS = {
   CALL_WS: '/call/',
